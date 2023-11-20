40b42e24594f4487ce4f5c313c0b8faaaf5f220f
khlee <lkhthug2@gmail.com>
2023-11-20 21:41:15 +0900

    init commit

---
+++ 01_DB/db_script.sql
@@ -0,0 +1,10 @@
+-- root 권한 실행
+
+
+CREATE DATABASE malldb;
+
+CREATE USER 'malldbuser'@'localhost' identified BY 'malldbuser';
+CREATE USER 'malldbuser'@'%' identified BY 'malldbuser';
+
+GRANT ALL PRIVILEGES ON malldb.* TO 'malldbuser'@'localhost';
+GRANT ALL PRIVILEGES ON malldb.* TO 'malldbuser'@'%';
---
+++ backend/mallapi/src/test/java/kr/co/lkhthug/mallapi/MallapiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.lkhthug.mallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
